e61359e994efc83d4067c03cb4573923e2840bf0
Marianne <macoam00@hotmail.com>
2019-10-03 07:29:45 -0700

    Ultimos cambios

--- InterfazDinamica/MainWindow.xaml.cs
+++ InterfazDinamica/MainWindow.xaml.cs
@@ -23,20 +23,49 @@ namespace InterfazDinamica
         public MainWindow()
         {
             InitializeComponent();
+
+            btnCancelar.Visibility = Visibility.Hidden;
+            btnGuardar.Visibility = Visibility.Hidden;
+
         }
 
         private void ComboxTitulo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             grdDatos.Children.Clear();
+        }
 
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
             switch (comboxTitulo.SelectedIndex)
             {
                 case 0: //Alumno
                     grdDatos.Children.Add(new Alumno());
+                    btnGuardar.Visibility = Visibility.Visible;
+                    btnCancelar.Visibility = Visibility.Visible;
+                    break;
+                case 1:  //Maestro
+                    grdDatos.Children.Add(new Maestro());
+                    btnGuardar.Visibility = Visibility.Visible;
+                    btnCancelar.Visibility = Visibility.Visible;
                     break;
                 default:
                     break;
+
             }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            grdDatos.Children.Clear();
+            btnGuardar.Visibility = Visibility.Hidden;
+            btnCancelar.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            grdDatos.Children.Clear();
+            btnGuardar.Visibility = Visibility.Hidden;
+            btnCancelar.Visibility = Visibility.Hidden;
         }
     }
 }
